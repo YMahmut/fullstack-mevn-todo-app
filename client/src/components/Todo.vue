7e63e51fcc0725ca9101dffa33090d611ad95921
Mahmut <softcandsoftware@Softcand-MacBook-Pro.local>
2022-11-19 23:52:43 +0300

    some bug fixed and ui restored

--- client/src/components/Todo.vue
+++ client/src/components/Todo.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="todo">
-    <div class="row">
-      <div class="add-todo-button-center" :class="{'col':todos.length>0}">
-        <NewTodo class="m-5 p-3" @sendNewToDo="AddNewTodoItem($event)"/>
+    <div class="row add-todo-button-initial">
+      <span class="col">
+        <div :class="{'col':todos.length>0}">
+            <input class="col-md-6 new-todo m-3 p-3" type="text" v-model="doTo" @keypress.enter="addNewTodo" placeholder="click and add new todo">
+            <button class="col-md-3 my-3 py-3 mx-5 px-5" @click="addNewTodo">Add</button>
       </div>
+      </span>
       <div class="col">
           <TransitionGroup name="list" tag="ul" >
             <li class="m-2 p-2" v-for="(todo,index) in todos" :key="todo._id">
               <div class="col-8">
                 <span  v-if="isEditMode && index === selectedEditItemIndex" class="col-md todos">
-                  <input class="todo-edit-input" type="text" v-model="selectedEditItem" />
+                  <input class="todo-edit-input" type="text" v-model="selectedEditItem" @keypress.enter="EditTodo(todo, index)" />
                 </span>
                 <span v-if="index !== selectedEditItemIndex"
                       @click="TodoCompletedControl(todo)"
@@ -19,12 +22,17 @@
               </div>
               <div class="col-4">
                 <span>
-                  <button v-if="!isEditMode"  class="col-xs m-2 p-2 del-button" @click="DeleteTodo(todo)">Delete</button>
-                  <button v-if="isEditMode  && index === selectedEditItemIndex"  class="col-xs m-2 p-2 del-button" @click="editCancelled()">Cancel</button>
+                  <button v-if="!isEditMode" :style="todo.completed ? {border:'3px solid green'} : {color:''}" class="col-xs m-2 p-2 del-button" @click="DeleteTodo(todo)">Delete</button>
+                  <button  v-if="isEditMode"
+                           :style="index !== selectedEditItemIndex? {opacity:'0'} : {opacity:'1'}"
+                           class="col-xs m-2 p-2 del-button" @click="editCancelled(index)">Cancel</button>
                 </span>
                 <span>
                   <button v-if="!isEditMode" class="col-xs m-2 p-2 edit-button" @click="startToEdit(todo)">Edit</button>
-                  <button v-if="isEditMode" class="col-xs m-2 p-2 edit-button" @click="EditTodo(todo)">Edit</button>
+                  <button v-if="isEditMode"
+                          :style="index !== selectedEditItemIndex? {opacity:'0'} : {opacity:'1'}"
+                          class="col-xs m-2 p-2 edit-button"
+                          @click="EditTodo(todo, index)">Edit</button>
                 </span>
               </div>
             </li>
@@ -37,14 +45,10 @@
 </template>
 
 <script>
-import NewTodo from "./NewTodo"
 import axios from "axios"
 
 
 export default {
-  components: {
-    NewTodo
-  },
   name: "ToDo",
   data() {
     return {
@@ -53,9 +57,16 @@ export default {
       todos: [],
       selectedEditItem:"",
       selectedEditItemIndex:null,
+      doTo:"",
     }
   },
   methods: {
+    addNewTodo(){
+      const isEmpty = this.doTo.replace(/[^a-z0-9]/gi,'');
+      if (!isEmpty.length) return;
+      this.AddNewTodoItem(this.doTo);
+      this.doTo="";
+    },
     DeleteTodo(todo) {
       this.todos.splice(this.todos.findIndex(t => t._id === todo._id), 1);
       this.todoDeleteItem(todo);
@@ -66,14 +77,16 @@ export default {
       this.selectedEditItemIndex=index;
       this.selectedEditItem=this.todos[index].description;
     },
-    EditTodo(todo) {
+    EditTodo(todo, index) {
+      if(this.selectedEditItemIndex !== index) return;
       this.todos[this.selectedEditItemIndex].description=this.selectedEditItem;
       this.selectedEditItemIndex=null;
       this.isEditMode=false;
       this.selectedEditItem="";
       this.todoEditItem(todo);
     },
-    editCancelled() {
+    editCancelled(index) {
+      if(this.selectedEditItemIndex !== index) return;
       this.selectedEditItemIndex=null;
       this.isEditMode=false;
       this.selectedEditItem="";
@@ -94,6 +107,15 @@ export default {
       await axios.delete(`/api/todoLists/${todo._id}`);
     },
     async todoEditItem(todo) {
+      const isEmpty = todo.description.replace(/[^a-z0-9]/gi,'');
+      console.log("isEmpty",isEmpty)
+      if (!isEmpty.length) {
+        setTimeout(()=>{
+          location.reload();
+        },50);
+        return;
+      }
+      console.log("todo.description",todo.description)
       await axios.put(`/api/todoLists/${todo._id}`,{description: todo.description, completed: todo.completed});
     }
   },
@@ -111,11 +133,8 @@ export default {
   width: 100%;
 }
 
-.add-todo-button-center {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  text-align: center;
+.add-todo-button-initial {
+  align-items: unset !important;
 }
 ol, ul {
   padding-left: 0rem !important;
@@ -147,10 +166,12 @@ li {
   border-radius: 5px;
   background-color: #f3ebeb;
   width: 60px;
+  border: 3px solid #c57272;
 }
 .todo-edit-input{
-  border: none;
+  border: 2px solid green;
   border-radius: 5px;
+  padding-left:10px ;
 }
 li > span {
   flex-direction: column;
@@ -171,5 +192,32 @@ li > span {
 .list-leave-to {
   opacity: 0;
   transform: translateX(40px);
+}
+
+.new-todo{
+  background: #f3ebeb;
+  border: 3px solid #c57272;
+  border-radius: 5px;
+  box-shadow: 1px 0px 3px;
+  color: black;
+}
+.new-todo:focus {
+  outline: none !important;
+  border:3px solid green;
+  box-shadow: 0 0 10px #719ECE;
+}
+button{
+  width: fit-content;
+  background-color: #c57272;
+  border: none;
+  border-radius: 5px;
+  box-shadow: 1px 2px 3px;
+}
+.row{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
 }
 </style>

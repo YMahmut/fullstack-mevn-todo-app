e2ffea6fa54cb51a240dbc57d57693d763f76f84
Mahmut <softcandsoftware@Softcand-MacBook-Pro.local>
2022-11-20 23:25:49 +0300

    file relocated

--- api/models/TodoList.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model}=require('mongoose');
-
-const TodoListSchema=new Schema({
-    description:{type:String, require:true},
-    completed:{type:Boolean,default: false}
-});
-
-const TodoList=model('todoList',TodoListSchema);
-module.exports=TodoList;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const morgan=require('morgan');
 const bodyParser=require('body-parser');
-const todoListItemsRoutes=require('./api/services/todoLists')
+const todoListItemsRoutes=require('./api/todoLists')
 
 app.use(cors());
 app.use(morgan('tiny'));
